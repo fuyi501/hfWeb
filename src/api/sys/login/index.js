65ac953bcc8653c81cc280f486b421b75458501b
fuyi501 <2516800961@qq.com>
2019-01-18 14:27:08 +0800

    9.15修改为100.240

--- src/api/sys/login/index.js
+++ src/api/sys/login/index.js
@@ -4,7 +4,7 @@ export function AccountLogin (data) {
   console.log('accountLogin')
   return request({
     // url: '/login',
-    url: 'http://192.168.9.15:8360/login/login',
+    url: 'http://192.168.100.240:8360/login/login',
     method: 'post',
     data
   })
